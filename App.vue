f9b8ee87730d4d24d5a59492a228ece1a9aba9f8
ImeneSb <115999375+imene18@users.noreply.github.com>
2022-12-14 14:36:02 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,51 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import layout from './layout.vue'
+import experiences from './components/experiences.vue'
+import centredinterets from './components/centredinterets.vue'
+import qualitesPro from './components/qualitesPro.vue'
+import aboutme from './components/aboutme.vue'
+import image from "/Users/imenesebai/vite-project/IMG_5132 Small.jpeg"
+import { useQuasar } from 'quasar'
+import competences from './components/competences.vue'
+import formation from './components/formation.vue'
+
+
+export default {
+  props: {
+  },
+  methods: {
+  },
+  components: {
+    layout,
+    aboutme,
+    image,
+    centredinterets,
+    experiences,
+    qualitesPro,
+    competences,
+    formation,
+  },
+  setup () {
+    const $q = useQuasar()
+
+    // calling here; equivalent to when component is created
+    $q.dark.set(false)
+  }
+}
+</script>
+
+<template>
+  <div >
+    <layout msg="CV Imène SEBAI"/>
+    <aboutme msg="CV Imène SEBAI"/>
+    <experiences msg="CV Imène SEBAI"/>
+    <formation msg="CV Imène SEBAI"/>
+    <competences msg="CV Imène SEBAI"/>
+    <centredinterets msg="CV Imène SEBAI"/>
+    <qualitesPro msg="CV Imène SEBAI"/>
+
+
+</div> 
+</template>
---
+++ formation.vue
@@ -0,0 +1,97 @@
+<template>
+<div class="q-pa-md">
+<q-toolbar class="text-white rounded-borders justify-center" style="background: radial-gradient(circle, #a36ca3 0%, #a36ca3 100%)"></q-toolbar></div>
+  <div class="q-pa-md q-gutter-sm">
+    <q-tree
+      :nodes="customize"
+      node-key="label"
+      default-expand-all
+    >
+
+      <template v-slot:default-header="prop">
+        <div class="row items-center text-h6">
+          <q-icon :name="prop.node.icon || 'share'" color="accent" size="28px" class="q-mr-sm" />
+          <div class="text-weight-bold text-wight">{{ prop.node.label }}</div>
+        </div>
+      </template>
+
+      <template v-slot:default-body="prop">
+        <div v-if="prop.node.story">
+          <span class="text-weight-bold"></span> {{ prop.node.story }}
+        </div>
+        <span v-else class="text-weight-light text-black"></span>
+      </template>
+    </q-tree>
+  </div>
+<div class="q-pa-md">
+<q-toolbar class="text-white rounded-borders justify-center" style="background: radial-gradient(circle, #a36ca3 0%, #a36ca3 100%)"></q-toolbar></div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const count = ref(0)
+export default {
+  setup () {
+    return {
+      customize: [
+        {
+          label: 'Formations',
+          icon: 'school',
+          header: 'root',
+          children: [
+            {
+              label: 'Licence Science pour la Santé spécialité Objets Connectés Université Claude Bernard Lyon 1 / 2020-2023',
+              icon: 'settings_input_antenna',
+              header: 'generic',
+              children: [
+                {
+                  label: 'UE Informatique et réseau appliquée à la santé',
+                  header: 'generic',
+                  body: 'story',
+                  story: 'programmation (logiciel pour doser l’insuline, principe de l’échographe, web, Javascript, Vue), modélisation en UML, SQL'
+                },
+                {
+                  label: 'UE Data Science',
+                  body: 'story',
+                  story: 'manipulation, prédiction, analyse de données, réseaux de neurones'
+                },
+                {
+                  label: 'UE Mathématiques et statistiques appliquées à la santé',
+                  body: 'story',
+                },
+                {
+                  label: 'UE Développement et maintenance des dispositifs médicaux',
+                  body: 'story',
+                },
+                {
+                  label: 'UE Neurosciences',
+                  body: 'story',
+                },
+                {
+                  label: 'UE Outils diagnostiques et physiopathologiques',
+                  body: 'story',
+                },
+              ]
+            },
+            {
+              label: 'Première année communes aux études de santé Université Claude Bernard Lyon 1 / 2019-2020',
+              header: 'generic',
+              body: 'toggle',
+              caption: 'Why are we as consumers so captivated by stories of great customer service? Perhaps it is because...',
+              enabled: false,
+              icon: 'medical_services',
+            
+            },
+            {
+              label: 'Baccalauréat Scientifique Lycée Parc Chabrières Oullins / 2019',
+              icon: 'workspace_premium',
+              
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
